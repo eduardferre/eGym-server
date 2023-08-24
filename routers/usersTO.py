09c5731457413a29d042aa0914ee18a20afde9c2
eduardferre <eduard.ferre.sanchez@estudiantat.upc.edu>
2023-08-25 00:38:42 +0200

    status codes usersTO router

--- routers/usersTO.py
+++ routers/usersTO.py
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/usersTO",
                    tags=["usersTO"],
                    responses={status.HTTP_404_NOT_FOUND: { "message": "Not found" }})
 
-@router.get("/", response_model=list[UserTO])
+@router.get("/", response_model=list[UserTO], status_code=status.HTTP_200_OK)
 async def getUsersTO(): #NOSONAR
     query = f"SELECT * FROM dbo.users" #NOSONAR
     sql_cursor.execute(query)
@@ -20,14 +20,14 @@ async def getUsersTO(): #NOSONAR
     user = sql_cursor.fetchone()
     
     if user == None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="There are no users in the database")
+        raise HTTPException(status_code=status.HTTP_204_NO_CONTENT, detail="There are no users in the database")
     
     while user:
         users_list.append(UserTO(**userTO_schema(tupleUserTOToDict(user))))
         user = sql_cursor.fetchone()
     return users_list
 
-@router.get("/{id}", response_model=UserTO)
+@router.get("/{id}", response_model=UserTO, status_code=status.HTTP_200_OK)
 async def getUserTOById(id: int): #NOSONAR
     query = f"SELECT * FROM dbo.users WHERE userId={id}"
     sql_cursor.execute(query)
@@ -38,10 +38,10 @@ async def getUserTOById(id: int): #NOSONAR
     
     return UserTO(**userTO_schema(tupleUserTOToDict(user)))
 
-@router.post("/", response_model=UserTO, status_code=201)
+@router.post("/", response_model=UserTO, status_code=status.HTTP_201_CREATED)
 async def postUserTO(userTO: UserTO):
     if type(searchUserTO("username", userTO.username)) == UserTO:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"The user with username = {userTO.username} already exists")
+        raise HTTPException(status_code=status.HTTP_226_IM_USED, detail=f"The user with username = {userTO.username} already exists")
     
     query = f"INSERT dbo.users (username, firstName, lastName, email, password, birthDate)\
                 OUTPUT INSERTED.*\
@@ -51,12 +51,12 @@ async def postUserTO(userTO: UserTO):
     sqlserver_client.commit()
     return UserTO(**userTO_schema(tupleUserTOToDict(user)))
 
-@router.put("/", response_model=UserTO, status_code=201)
+@router.put("/", response_model=UserTO, status_code=status.HTTP_201_CREATED)
 async def updateUserTO(user_list: list[UserTO]):
     userTO_original, userTO_update = user_list[0], user_list[1]
     user_search = searchUserTO("username", userTO_original.username)
     if type(user_search) != UserTO:
-        raise HTTPException(status_code=404, detail=user_search["error"])
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=user_search["error"])
     
     query = f"UPDATE dbo.users\
                 SET username = '{userTO_update.username}', firstName = '{userTO_update.firstname}', lastName = '{userTO_update.lastname}', email = '{userTO_update.email}', password = '{userTO_update.password}', birthDate = '{userTO_update.birthDate}'\
@@ -67,11 +67,11 @@ async def updateUserTO(user_list: list[UserTO]):
     sqlserver_client.commit()
     return UserTO(**userTO_schema(tupleUserTOToDict(user)))
 
-@router.delete("/{username}", response_model=UserTO, status_code=201)
+@router.delete("/{username}", response_model=UserTO, status_code=status.HTTP_200_OK)
 async def deleteUserTO(username: str):
     user_search = searchUserTO("username", username)
     if type(user_search) != UserTO:
-        raise HTTPException(status_code=404, detail=user_search["error"])
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=user_search["error"])
     
     query = f"DELETE FROM dbo.users\
                 OUTPUT DELETED.*\
